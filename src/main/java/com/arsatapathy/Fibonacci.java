ae809574d686fbc7cfbfe031e94ff75839dd1d1c
arsatapathy <ashish.satapathy2014@gmail.com>
2022-01-16 18:23:49 +0530

    Fibonacci

--- src/main/java/com/arsatapathy/Fibonacci.java
+++ src/main/java/com/arsatapathy/Fibonacci.java
@@ -1,11 +1,24 @@
 package com.arsatapathy;
 
+import java.util.Map;
+
 public class Fibonacci {
 
     // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
-    public static int fib(int nth) {
+    // O(2^n)
+    public static long fib(long nth) {
         if (nth == 1 || nth == 2) return 1;
 
         return fib(nth - 1) + fib(nth - 2);
+    }
+
+    // Memoization - O(n)
+    public static long fib(long nth, Map<Long, Long> memo) {
+        if (memo.containsKey(nth)) return memo.get(nth);
+        if (nth == 1 || nth == 2) return 1;
+
+        long value = fib(nth - 1, memo) + fib(nth -2, memo);
+        memo.put(nth,value);
+        return value;
     }
 }
--- src/test/java/com/arsatapathy/FibonacciTest.java
+++ src/test/java/com/arsatapathy/FibonacciTest.java
@@ -3,10 +3,19 @@ package com.arsatapathy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class FibonacciTest {
 
     @Test
     public void test_01() {
         Assertions.assertEquals(3, Fibonacci.fib(4));
+        Assertions.assertEquals(12586269025L, Fibonacci.fib(50));
+    }
+
+    @Test
+    public void test_02() {
+        Assertions.assertEquals(55, Fibonacci.fib(10, new HashMap<>()));
+        Assertions.assertEquals(12586269025L, Fibonacci.fib(50, new HashMap<>()));
     }
 }
