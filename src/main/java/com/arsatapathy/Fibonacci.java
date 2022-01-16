3b07c43020a2beaad416aadfc26001b88542ffe2
arsatapathy <ashish.satapathy2014@gmail.com>
2022-01-16 09:01:00 +0530

    Fibonacci

--- src/main/java/com/arsatapathy/Fibonacci.java
+++ src/main/java/com/arsatapathy/Fibonacci.java
@@ -2,7 +2,7 @@ package com.arsatapathy;
 
 public class Fibonacci {
 
-    // 1, 1, 2, 3, 4, 8, 13, 21, 34, 55, 89, 144
+    // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
     public static int fib(int nth) {
         if (nth == 1 || nth == 2) return 1;
 
