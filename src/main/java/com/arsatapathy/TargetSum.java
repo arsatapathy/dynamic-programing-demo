08fe0284a54ffc589826b568d35fcf54f6ef1aa8
arsatapathy <ashish.satapathy2014@gmail.com>
2022-02-22 15:55:52 +0530

    TargetSum

--- src/main/java/com/arsatapathy/TargetSum.java
+++ src/main/java/com/arsatapathy/TargetSum.java
@@ -6,13 +6,12 @@ public class TargetSum {
 
     public static boolean canSum(int targetSum, int[] numbers) {
         if (targetSum == 0) return true;
-        if (targetSum < 0) return false;
+        if (targetSum <= -1) return false;
 
-        for (int number:
-             numbers) {
-            int reminder = targetSum - number;
+        for (int num : numbers) {
+            int rem = targetSum - num;
 
-            if (canSum(reminder, numbers)) {
+            if (canSum(rem, numbers)) {
                 return true;
             }
         }
@@ -21,19 +20,17 @@ public class TargetSum {
     }
 
     public static boolean canSum(int targetSum, int[] numbers, Map<Integer, Boolean> memo) {
-
         if (memo.containsKey(targetSum)) return memo.get(targetSum);
 
         if (targetSum == 0) return true;
-        if (targetSum < 0) return false;
+        if (targetSum <= -1) return false;
 
-        for (int number:
-                numbers) {
-            int reminder = targetSum - number;
+        for (int num : numbers) {
+            int rem = targetSum - num;
 
-            if (canSum(reminder, numbers)) {
-                memo.put(targetSum, true);
-                return  true;
+            if (canSum(rem, numbers, memo)) {
+                memo.put(rem, true);
+                return true;
             }
         }
 
